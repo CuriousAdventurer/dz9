c0168ccfbc70d7bdaaa9a8390dfc6e12ad02669c
CuriousAdventurer <138018696+CuriousAdventurer@users.noreply.github.com>
2023-10-10 23:31:31 +0300

    Add files via upload

---
+++ zd64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Задайте финальное значение промежутка: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(num, count);
+
+{
+    if (num<1)
+    {
+    Console.Write("Натуральное число не может быть меньше 1");
+    }
+}
+    
+void NaturalToLow(int a, int count)
+{
+    if (count > a)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(a, count + 1);
+        Console.Write(count + " ");
+    }
+}
---
+++ zd66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите стартовое число M: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите финальное число N: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(a, b);
+
+void SumFromMToN(int a, int b)
+{
+    Console.Write(SumMN(a - 1, b));
+}
+int SumMN(int a, int b)
+{
+    int result = a;
+    if (a == b)
+        return 0;
+    else
+    {
+        a++;
+        result = a + SumMN(a, b);
+        return result;
+    }
+}
+
